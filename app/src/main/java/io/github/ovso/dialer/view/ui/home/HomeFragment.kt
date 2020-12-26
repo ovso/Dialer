1b2ec413ab3092c2ce90fe251b115e33045e810b
ovso <ovsoce@gmail.com>
2020-12-26 20:13:58 +0900

    feat: Setup toolbar, drawer layout

--- app/src/main/java/io/github/ovso/dialer/view/ui/home/HomeFragment.kt
+++ app/src/main/java/io/github/ovso/dialer/view/ui/home/HomeFragment.kt
@@ -1,5 +1,9 @@
 package io.github.ovso.dialer.view.ui.home
 
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.viewModels
 import io.github.ovso.dialer.R
 import io.github.ovso.dialer.databinding.FragmentHomeBinding
@@ -7,5 +11,34 @@ import io.github.ovso.dialer.view.base.DataBindingFragment
 
 class HomeFragment : DataBindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
   override val viewModel: HomeViewModel by viewModels()
+
+  private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+
+  override fun onStart() {
+    super.onStart()
+    setHasOptionsMenu(true)
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    setupToolbarAndDrawer()
+  }
+
+  private fun setupToolbarAndDrawer() {
+    (requireActivity() as? AppCompatActivity)?.let {
+      it.setSupportActionBar(binding.includeToolbar.toolbar)
+      it.title = "Speed Dialer"
+    }
+    actionBarDrawerToggle = ActionBarDrawerToggle(
+      activity,
+      binding.drawerLayout,
+      binding.includeToolbar.toolbar,
+      R.string.navigation_drawer_open,
+      R.string.navigation_drawer_close
+    ).also {
+      it.syncState()
+      binding.drawerLayout.addDrawerListener(it)
+    }
+  }
 
 }
