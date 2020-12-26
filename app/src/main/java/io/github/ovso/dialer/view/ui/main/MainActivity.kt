1ac3ec6f89e1844e546848b1689546c3163d74de
ovso <ovsoce@gmail.com>
2020-12-26 13:36:58 +0900

    feat: Navigate test

--- app/src/main/java/io/github/ovso/dialer/view/ui/main/MainActivity.kt
+++ app/src/main/java/io/github/ovso/dialer/view/ui/main/MainActivity.kt
@@ -1,11 +1,9 @@
 package io.github.ovso.dialer.view.ui.main
 
 import android.os.Bundle
-import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
@@ -15,13 +13,12 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
-import com.google.android.material.snackbar.Snackbar
 import io.github.ovso.dialer.R
 import io.github.ovso.dialer.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(R.layout.activity_main), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity(),
+  NavigationView.OnNavigationItemSelectedListener {
   private lateinit var binding: ActivityMainBinding
 
   private lateinit var appBarConfiguration: AppBarConfiguration
@@ -29,8 +26,9 @@ class MainActivity : AppCompatActivity(R.layout.activity_main), NavigationView.O
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    val toolbar = findViewById<Toolbar>(R.id.toolbar)
-    setSupportActionBar(findViewById(R.id.toolbar))
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+    setSupportActionBar(binding.appBarMain.toolbar)
     supportActionBar?.setDisplayShowCustomEnabled(true)
     val navHostFragment =
       supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
@@ -38,26 +36,26 @@ class MainActivity : AppCompatActivity(R.layout.activity_main), NavigationView.O
     appBarConfiguration = AppBarConfiguration(findNavController.graph)
     setupActionBarWithNavController(findNavController, appBarConfiguration)
 
-    val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
     val toggle = ActionBarDrawerToggle(
       this,
-      drawerLayout, toolbar,
+      binding.drawerLayout, binding.appBarMain.toolbar,
       R.string.navigation_drawer_open,
       R.string.navigation_drawer_close
     )
-    drawerLayout.addDrawerListener(toggle)
+    binding.drawerLayout.addDrawerListener(toggle)
     toggle.syncState()
-    findViewById<NavigationView>(R.id.nav_view).setNavigationItemSelectedListener(this)
-
+    binding.navView.setNavigationItemSelectedListener(this)
+    binding.appBarMain.fab.setOnClickListener {
+      findNavController.navigate(R.id.nav_gallery)
+    }
   }
 
   override fun onSupportNavigateUp(): Boolean {
-    val navController = findNavController(R.id.nav_host_fragment_content_main)
-    return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    return findNavController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
   }
 
   override fun onNavigationItemSelected(item: MenuItem): Boolean {
-    findViewById<DrawerLayout>(R.id.drawer_layout).closeDrawer(GravityCompat.START)
+//    binding.drawerLayout.closeDrawer(GravityCompat.START)
     return true
   }
 }
