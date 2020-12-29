813e2df3eea36d9d1b5263f2055dc7c012eab0c3
ovso <ovsoce@gmail.com>
2020-12-30 08:22:04 +0900

    feat: Apply viewBinding on EasyColorPicker.kt

--- colorpicker/src/main/java/io/github/ovso/colorpicker/EasyColorPicker.kt
+++ colorpicker/src/main/java/io/github/ovso/colorpicker/EasyColorPicker.kt
@@ -3,8 +3,9 @@ package io.github.ovso.colorpicker
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import android.view.ViewGroup
+import android.view.View
 import android.widget.FrameLayout
+import androidx.annotation.ColorRes
 import io.github.ovso.colorpicker.databinding.ContainerBinding
 
 class EasyColorPicker @JvmOverloads constructor(
@@ -17,12 +18,13 @@ class EasyColorPicker @JvmOverloads constructor(
   defStyle
 ) {
 
+  private lateinit var view: View
 
+  @ColorRes
   var color: Int? = null
     set(value) {
       field = value
-      invalidate()
-      requestLayout()
+      binding.root.setBackgroundColor(value!!)
     }
 
   init {
@@ -31,23 +33,13 @@ class EasyColorPicker @JvmOverloads constructor(
 
   private lateinit var binding: ContainerBinding
   private fun init(attrs: AttributeSet?, defStyle: Int) {
-    // Load attributes
-    binding = ContainerBinding.inflate(LayoutInflater.from(context), this, false)
+    binding = ContainerBinding.inflate(LayoutInflater.from(context), this, true)
     val typeArray = context.obtainStyledAttributes(
       attrs, R.styleable.EasyColorPicker, defStyle, 0
     )
     color = typeArray.getInt(R.styleable.EasyColorPicker_bgColor, 0x000000)
-
-
-    addView(binding.root, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
     binding.root.setBackgroundColor(color!!)
-
     typeArray.recycle()
-
-  }
-
-  fun setBg(bg: Int) {
-    binding.root.setBackgroundColor(bg)
   }
 
 }
