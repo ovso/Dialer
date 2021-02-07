9949a5c83cf4c61e5776815fde631934db092e37
ovso <ovsoce@gmail.com>
2021-02-07 19:21:54 +0900

    feat: Disable dark theme

--- app/src/main/java/io/github/ovso/dialer/app/DialerApplication.kt
+++ app/src/main/java/io/github/ovso/dialer/app/DialerApplication.kt
@@ -1,6 +1,7 @@
 package io.github.ovso.dialer.app
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.startup.AppInitializer
 import dagger.hilt.android.HiltAndroidApp
 import io.github.ovso.dialer.data.local.AppDatabase
@@ -10,6 +11,7 @@ class DialerApplication : Application() {
   lateinit var database: AppDatabase
   override fun onCreate() {
     super.onCreate()
+    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     database = AppInitializer
       .getInstance(this)
       .initializeComponent(DatabaseInitializer::class.java)
