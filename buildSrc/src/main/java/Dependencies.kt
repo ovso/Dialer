2168ef91a4e485b91956d66813a091bbb5a64511
jaeho.5 <ovsoce@gmail.com>
2023-04-06 21:07:19 +0900

    chore: 배포를 위한 버전업

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -5,8 +5,8 @@ object DefaultConfig {
   const val minSdk = 23
   const val targetSdk = 33
   const val appId = "io.github.ovso.dialer"
-  const val versionCode = 4
-  const val versionName = "1.0.3"
+  const val versionCode = 5
+  const val versionName = "1.0.4"
 
 }
 
